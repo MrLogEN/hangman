c46a6d5bbdbcd248da5425362848156f785324b5
Vilém Charwot <vilem.charwot@proton.me>
2025-03-25 00:17:07 +0100

    Create setup files for Linux and Windows

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@echo off
+
+SET PRE_COMMIT_SOURCE=.hooks\pre-commit
+SET PRE_COMMIT_TARGET=.git\hooks\pre-commit
+
+IF EXIST "%PRE_COMMIT_TARGET%" (
+	echo Removing existing file at %PRE_COMMIT_TARGET%
+	del "%PRE_COMMIT_TARGET%"	
+)
+
+echo Copying pre-commit hook from %PRE_COMMIT_SOURCE% to %PRE_COMMIT_TARGET%
+copy "%PRE_COMMIT_SOURCE%" "%PRE_COMMIT_TARGET%"
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# pre-commit hooks
+PRE_COMMIT_SOURCE="./.hooks/pre-commit"
+PRE_COMMIT_TARGET="./.git/hooks/pre-commit"
+
+# Remove any existing pre-commit hooks
+if [ -e "$PRE_COMMIT_TARGET" ]; then
+	echo "Removing existing file at $PRE_COMMIT_TARGET"
+	rm "$PRE_COMMIT_TARGET"
+fi
+
+# Create a new symlink
+cp "$PRE_COMMIT_SOURCE" "$PRE_COMMIT_TARGET"
+echo "File copied from $PRE_COMMIT_SOURCE to $PRE_COMMIT_TARGET"
+
