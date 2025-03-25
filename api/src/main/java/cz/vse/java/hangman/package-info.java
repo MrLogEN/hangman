3aad4c843fa27a5653d9795e47bc696f57b612f9
Vilém Charwot <vilem.charwot@proton.me>
2025-03-25 15:30:53 +0100

    Add package-info.java to modules

---
+++ api/src/main/java/cz/vse/java/hangman/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package serves as a client to Hangman game server.
+ *
+ * @author Vilém Charwot
+ * @author Vítězslav Setnička
+ * @version 1.0
+ */
+package cz.vse.java.hangman;
---
+++ server/src/main/java/cz/vse/java/hangman/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package is a server implementation for Hangman game.
+ *
+ * @author Vilém Charwot
+ * @version 1.0
+ */
+ package cz.vse.java.hangman;
