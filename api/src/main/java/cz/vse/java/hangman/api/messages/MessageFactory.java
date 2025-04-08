893d59a0c1ac7ea031353c67ecc068f44c58d6d7
Vilém Charwot <vilem.charwot@proton.me>
2025-04-08 23:57:01 +0200

    Create MessageFactory

---
+++ api/src/main/java/cz/vse/java/hangman/api/messages/MessageFactory.java
@@ -0,0 +1,10 @@
+package cz.vse.java.hangman.api.messages;
+
+import cz.vse.java.hangman.api.messages.serialization.MessageWrapper;
+/**
+ * This class provides methods for easy creation of concrete instances of {@link Message}
+ * wrapped in {@link MessageWrapper}
+ *
+ */
+public class MessageFactory {
+}
