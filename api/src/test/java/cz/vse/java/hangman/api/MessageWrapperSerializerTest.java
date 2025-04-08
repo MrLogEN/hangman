c5b6f94d65afd88540ffba6a552ba9515cf823f0
Vilém Charwot <vilem.charwot@proton.me>
2025-04-08 19:04:24 +0200

    Create a test for MessageWrapperSerializer

---
+++ api/src/test/java/cz/vse/java/hangman/api/MessageWrapperSerializerTest.java
@@ -0,0 +1,25 @@
+package cz.vse.java.hangman.api;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class MessageWrapperSerializerTest {
+
+	@Test
+	public void ShouldSerializeDummyMessage() {
+		Gson gson = new GsonBuilder()
+					.registerTypeAdapter(MessageWrapper.class, new MessageWrapperSerializer<>())
+					.create();
+		DummyMessage dummyMessage = new DummyMessage(10, "Some name", "Some desc");
+		MessageWrapper wrapper = new MessageWrapper<DummyMessage>(dummyMessage, DummyMessage.class);
+		String expected = "{\"type\":\"cz.vse.java.hangman.api.DummyMessage\","
+				+ "\"message\":{\"id\":10,\"name\":\"Some name\",\"desc\":\"Some desc\"}}";
+
+		String actual = gson.toJson(wrapper);
+
+		assertEquals(expected, actual);
+	}
+}
