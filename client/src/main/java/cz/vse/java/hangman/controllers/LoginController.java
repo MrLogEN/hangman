31e5da08c6c41df9b4203db35fce883834a7089f
Vítězslav Setnička <aacko12345@gmail.com>
2025-04-13 11:25:20 +0200

    Implementing Controller class for login page.

---
+++ client/src/main/java/cz/vse/java/hangman/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package cz.vse.java.hangman.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Button confirmButton;
+
+
+    @FXML
+    public void initialize() {
+        //TODO
+    }
+
+    @FXML
+    private void handleConfirmButton(ActionEvent actionEvent) {
+        //TODO
+    }
+}
