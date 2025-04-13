0d845f4052bb9e712bf45f5bf772d1697204a544
Vítězslav Setnička <aacko12345@gmail.com>
2025-04-13 11:25:56 +0200

    Implementing Controller class for rooms page.

---
+++ client/src/main/java/cz/vse/java/hangman/controllers/RoomsController.java
@@ -0,0 +1,32 @@
+package cz.vse.java.hangman.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class RoomsController {
+    @FXML
+    private Button createRoomButton;
+    @FXML
+    private Button refreshRoomListButton;
+    @FXML
+    private TableView roomsTable;
+    //TODO add a list of rooms after implementation
+
+
+    @FXML
+    public void initialize() {
+        //TODO
+    }
+
+    @FXML
+    private void handleRefreshRoomListButton(ActionEvent actionEvent) {
+        //TODO
+    }
+
+    @FXML
+    private void handleCreateRoomButton(ActionEvent actionEvent) {
+        //TODO
+    }
+}
