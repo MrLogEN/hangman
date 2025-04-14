a9da074862c28e6b9d47367714e052d2f0fe19de
Vilém Charwot <mrlogen1299@gmail.com>
2025-04-14 17:32:59 +0200

    Prepare RoomDTO method

--- api/src/main/java/cz/vse/java/hangman/api/dtos/RoomDTO.java
+++ api/src/main/java/cz/vse/java/hangman/api/dtos/RoomDTO.java
@@ -1,6 +1,12 @@
 package cz.vse.java.hangman.api.dtos;
 
+import cz.vse.java.hangman.api.Room;
+
 import java.util.Set;
 
 public record RoomDTO(String name, int maxPlayers, Set<PlayerDTO> playerDTOSet, PlayerDTO leader, GameDTO gameDTO) {
+    public static RoomDTO fromRoom(Room room) {
+        //#TODO: implement this method
+        return null;
+    }
 }
