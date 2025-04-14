09b90a59d36f506c45410fc488eeaffe8224749d
Vilém Charwot <mrlogen1299@gmail.com>
2025-04-14 11:18:54 +0200

    Update javadoc on Player

--- api/src/main/java/cz/vse/java/hangman/api/Player.java
+++ api/src/main/java/cz/vse/java/hangman/api/Player.java
@@ -1,5 +1,8 @@
 package cz.vse.java.hangman.api;
 
+/**
+ * Class representing a player in the game.
+ */
 public class Player {
     private String name;
     private ConnectingStatus connectingStatus;
@@ -8,20 +11,35 @@ public class Player {
         return connectingStatus;
     }
 
+    /**
+     * Enum representing the connection status of a player.
+     * This function is used on the server to determine
+     * if the player is connected, disconnected or reconnecting.
+     */
     public enum ConnectingStatus {
         CONNECTED,
         DISCONNECTED,
         RECONNECTING
     }
+
     public Player(String name) {
         this.name = name;
         this.connectingStatus = ConnectingStatus.CONNECTED;
     }
 
+    /**
+     * Getter for the name of the player.
+     * @return the name of the player.
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * Sets the {@link ConnectingStatus} of the player.
+     * @param connectingStatus the new connecting status of the player.
+     */
     public void setConnectingStatus(ConnectingStatus connectingStatus) {
         this.connectingStatus = connectingStatus;
     }
