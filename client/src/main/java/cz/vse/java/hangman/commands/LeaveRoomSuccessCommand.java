ceb0e5e983191e2df5409e6a5147e9169fbeed20
Vítězslav Setnička <aacko12345@gmail.com>
2025-04-26 08:59:25 +0200

    Creating all of the client commands responding to server messages.

---
+++ client/src/main/java/cz/vse/java/hangman/commands/LeaveRoomSuccessCommand.java
@@ -0,0 +1,10 @@
+package cz.vse.java.hangman.commands;
+
+import cz.vse.java.hangman.api.commands.Command;
+
+public class LeaveRoomSuccessCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ client/src/main/java/cz/vse/java/hangman/commands/StartGameSuccessCommand.java
@@ -0,0 +1,10 @@
+package cz.vse.java.hangman.commands;
+
+import cz.vse.java.hangman.api.commands.Command;
+
+public class StartGameSuccessCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
