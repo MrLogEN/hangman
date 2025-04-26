b8965e81e77af27f50daa7f876b78dc850d99085
Vítězslav Setnička <aacko12345@gmail.com>
2025-04-26 09:01:37 +0200

    Finished LoginController logic.

--- client/src/main/java/cz/vse/java/hangman/controllers/LoginController.java
+++ client/src/main/java/cz/vse/java/hangman/controllers/LoginController.java
@@ -1,24 +1,71 @@
 package cz.vse.java.hangman.controllers;
 
+
+import cz.vse.java.hangman.ClientHandler;
+import cz.vse.java.hangman.api.messages.client.ClientMessageFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+    @FXML
+    private Label messageLabel;
+
     @FXML
     private TextField usernameField;
-    @FXML
-    private Button confirmButton;
+
+
+
+    private ClientHandler clientHandler = new ClientHandler();
 
 
     @FXML
     public void initialize() {
-        //TODO
+        logger.info("Initializing LoginController screen");
+        messageLabel.setVisible(false);
+
+
     }
 
     @FXML
     private void handleConfirmButton(ActionEvent actionEvent) {
-        //TODO
+
+
+
+
+
+        String username = usernameField.getText();
+
+        logger.info("Checking, if the username field is empty.");
+        if (username.isEmpty()) {
+            logger.error("Username field is empty.");
+            messageLabel.setText("Prosím, zadejte uživatelské jméno.");
+        } else  {
+                clientHandler.send(ClientMessageFactory.createClientLoginMessage(username));
+
+        }
+
+
+
+
+
+
+    }
+
+    public ClientHandler getClientHandler() {
+        return clientHandler;
+    }
+
+    public void setClientHandler(ClientHandler clientHandler) {
+        this.clientHandler = clientHandler;
+    }
+
+    public Label getMessageLabel() {
+        return messageLabel;
     }
 }
