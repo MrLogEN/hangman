ed7089b0873c9f2e9598d7de05e3b95445d938a0
Vítězslav Setnička <aacko12345@gmail.com>
2025-04-28 08:13:55 +0200

    Implementing Command to handle Login Failure (existing name).

--- client/src/main/java/cz/vse/java/hangman/commands/LoginFailureCommand.java
+++ client/src/main/java/cz/vse/java/hangman/commands/LoginFailureCommand.java
@@ -1,10 +1,25 @@
 package cz.vse.java.hangman.commands;
 
 import cz.vse.java.hangman.api.commands.Command;
+import cz.vse.java.hangman.controllers.ControllerRegistry;
+import cz.vse.java.hangman.controllers.LoginController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginFailureCommand implements Command {
+    LoginController loginController = ControllerRegistry.getInstance().getLoginController();
+    private static final Logger logger = LoggerFactory.getLogger(LoginFailureCommand.class);
+
+
+
+
     @Override
     public void execute() {
 
+        //TODO nepodarilo se napojit na server
+            logger.info("Login failed. Username already exists.");
+            loginController.getMessageLabel().setVisible(true);
+            loginController.getMessageLabel().setText("Uživatelské jméno již existuje.");
+            logger.info("Waiting for user to enter a new username.");
     }
 }
