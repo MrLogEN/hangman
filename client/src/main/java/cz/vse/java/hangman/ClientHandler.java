a95715b0c90d064e4a46c227ac9182d00820a288
Vítězslav Setnička <aacko12345@gmail.com>
2025-05-20 14:42:11 +0200

    Creating handler for connection

--- client/src/main/java/cz/vse/java/hangman/ClientHandler.java
+++ client/src/main/java/cz/vse/java/hangman/ClientHandler.java
@@ -2,11 +2,11 @@ package cz.vse.java.hangman;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import cz.vse.java.hangman.commands.ClientCommandFactory;
 import cz.vse.java.hangman.api.messages.Message;
 import cz.vse.java.hangman.api.messages.serialization.MessageWrapper;
 import cz.vse.java.hangman.api.messages.serialization.MessageWrapperDeserializer;
 import cz.vse.java.hangman.api.messages.serialization.MessageWrapperSerializer;
+import cz.vse.java.hangman.commands.ClientCommandFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +16,11 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+/**
+ * ClientHandler class is responsible for managing the connection to the server.
+ * It handles sending and receiving messages to and from the server.
+ * It also deserializes incoming messages and executes the appropriate commands based on the message type.
+ */
 public class ClientHandler implements Runnable{
     private Socket socket;
     private BufferedReader in;
@@ -24,32 +29,57 @@ public class ClientHandler implements Runnable{
     private static final Logger logger = LoggerFactory.getLogger(ClientHandler.class);
 
 
+    /**
+     * Method that connects client to server.
+     *
+     * @param host the host
+     * @param port the port
+     * @throws IOException the io exception
+     */
     public void connect(String host, int port) throws IOException {
         socket = new Socket(host, port);
+        logger.info("Connected to server {}", socket);
         in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        logger.info("Input stream initialized.");
         out = new PrintWriter(socket.getOutputStream(), true);
+        logger.info("Output stream initialized.");
 
         gson = new GsonBuilder()
                 .registerTypeAdapter(MessageWrapper.class, new MessageWrapperDeserializer())
                 .registerTypeAdapter(MessageWrapper.class, new MessageWrapperSerializer())
                 .create();
+        logger.info("Gson {} initialized.", gson);
 
         new Thread(this).start();
+        logger.info("ClientHandler thread started.");
     }
 
+    /**
+     * Method that listens for incoming messages from the server, deserializes and executes incoming messages.
+     *
+     */
     private void listen() {
         try {
             String line;
             while ((line = in.readLine()) != null) {
+                logger.info("Received message: {}", line);
                 MessageWrapper wrapper = gson.fromJson(line, MessageWrapper.class);
+                logger.info("Deserialized message: {}", wrapper);
                 ClientCommandFactory commandFactory = new ClientCommandFactory();
                 commandFactory.fromMessage(wrapper.message()).execute();
+                logger.info("Executing command for message: {}", wrapper.message());
             }
         } catch (IOException e) {
             e.printStackTrace();
+            logger.error("Error while reading from server: {}", e.getMessage());
         }
     }
 
+    /**
+     * Method to send messages to the server.
+     *
+     * @param message the message
+     */
     public void send(Message message) {
         MessageWrapper wrapper = new MessageWrapper(message, message.getClass());
         String json = gson.toJson(wrapper);
@@ -58,12 +88,10 @@ public class ClientHandler implements Runnable{
     }
 
 
-    public void simulateIncomingMessage(Message message) {
-        ClientCommandFactory commandFactory = new ClientCommandFactory();
-        commandFactory.fromMessage(message).execute();
-    }
 
-
+    /**
+     * Method that starts the listening process.
+     */
     @Override
     public void run() {
         listen();
