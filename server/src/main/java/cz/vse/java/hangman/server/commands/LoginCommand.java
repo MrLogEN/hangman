0c6eacf77cd1987d228c79b3973ee0d51e38e247
Vilém Charwot <vilem.charwot@proton.me>
2025-06-22 15:38:23 +0200

    Create LoginCommand

--- server/src/main/java/cz/vse/java/hangman/server/commands/LoginCommand.java
+++ server/src/main/java/cz/vse/java/hangman/server/commands/LoginCommand.java
@@ -1,7 +1,12 @@
 package cz.vse.java.hangman.server.commands;
 
+import cz.vse.java.hangman.api.Player;
 import cz.vse.java.hangman.api.commands.Command;
+import cz.vse.java.hangman.api.messages.Message;
 import cz.vse.java.hangman.api.messages.client.request.ClientLoginMessage;
+import cz.vse.java.hangman.api.messages.server.ServerMessageFactory;
+import cz.vse.java.hangman.api.messages.server.response.ServerLoginFailureMessage;
+import cz.vse.java.hangman.api.messages.server.response.ServerLoginSuccessMessage;
 import cz.vse.java.hangman.server.ClientHandler;
 import cz.vse.java.hangman.server.RoomManager;
 
@@ -24,7 +29,19 @@ public class LoginCommand implements Command {
 
     @Override
     public void execute() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+        String playerName = message.name();
+        Player player = roomManager.findPlayer(playerName);
+        Message response;
+        if(player != null) {
+            response = ServerMessageFactory.createServerLoginFailureMessage("Player " + playerName + " is alredy logged in.");
+        }
+        else {
+            player = new Player(playerName);
+            roomManager.addNewPlayer(player);
+            handler.setPlayer(player);
+            response = ServerMessageFactory.createServerLoginSuccessMessage(player);
+        }
+
+        handler.addMessageToQueue(response);
     }
 }
