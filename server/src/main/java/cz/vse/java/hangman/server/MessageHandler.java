96e9e2804686141ddc0a64d7e2328ab9e802c5e4
Vilém Charwot <vilem.charwot@proton.me>
2025-06-22 11:52:53 +0200

    Implement factory methods for all client commands

--- server/src/main/java/cz/vse/java/hangman/server/MessageHandler.java
+++ server/src/main/java/cz/vse/java/hangman/server/MessageHandler.java
@@ -8,17 +8,23 @@ import cz.vse.java.hangman.api.commands.Command;
 import cz.vse.java.hangman.api.commands.CommandFactory;
 import cz.vse.java.hangman.api.messages.Message;
 import cz.vse.java.hangman.api.messages.client.request.*;
-import cz.vse.java.hangman.api.messages.serialization.MessageWrapper;
+import cz.vse.java.hangman.server.commands.JoinRoomCommand;
+import cz.vse.java.hangman.server.commands.LeaveRoomCommand;
+import cz.vse.java.hangman.server.commands.ListRoomsCommand;
 import cz.vse.java.hangman.server.commands.LoginCommand;
+import cz.vse.java.hangman.server.commands.StartGameCommand;
+import cz.vse.java.hangman.server.commands.TakeGuessCommand;
 
 public class MessageHandler implements CommandFactory {
 
     private final RoomManager roomManager;
+    private final ClientHandler handler;
     private final Map<Class<?>, Function<Message, Command>> commandFactories = new HashMap<>();
 
-    public MessageHandler(RoomManager roomManager){
+    public MessageHandler(RoomManager roomManager, ClientHandler handler){
         this.roomManager = roomManager;
-        commandFactories.put(ClientLoginMessage.class, message -> new LoginCommand((ClientLoginMessage) message, roomManager));
+        this.handler = handler;
+        registerFactories();
     }
 
     @Override
@@ -30,5 +36,14 @@ public class MessageHandler implements CommandFactory {
         else {
             throw new UnsupportedOperationException("Unsupported message type: "+ message.getClass().getName());
         }
+    }
+
+    private void registerFactories() {
+        commandFactories.put(ClientLoginMessage.class, message -> new LoginCommand((ClientLoginMessage) message, roomManager, handler));
+        commandFactories.put(ClientJoinRoomMessage.class, message -> new JoinRoomCommand((ClientJoinRoomMessage) message, roomManager, handler));
+        commandFactories.put(ClientLeaveRoomMessage.class, message -> new LeaveRoomCommand((ClientLeaveRoomMessage) message, roomManager, handler));
+        commandFactories.put(ClientListRoomsMessage.class, message -> new ListRoomsCommand((ClientListRoomsMessage) message, roomManager, handler));
+        commandFactories.put(ClientStartGameMessage.class, message -> new StartGameCommand((ClientStartGameMessage) message, roomManager, handler));
+        commandFactories.put(ClientTakeGuessMessage.class, message -> new TakeGuessCommand((ClientTakeGuessMessage) message, roomManager, handler));
     }
 }
