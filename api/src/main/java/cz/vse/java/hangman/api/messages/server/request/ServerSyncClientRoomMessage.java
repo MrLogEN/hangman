964cd81b078f076373b5801a627617d1b1539671
Vítězslav Setnička <aacko12345@gmail.com>
2025-06-23 06:37:13 +0200

    Zmenicka

--- api/src/main/java/cz/vse/java/hangman/api/messages/server/request/ServerSyncClientRoomMessage.java
+++ api/src/main/java/cz/vse/java/hangman/api/messages/server/request/ServerSyncClientRoomMessage.java
@@ -5,7 +5,7 @@ import cz.vse.java.hangman.api.messages.Message;
 
 /**
  * This message is sent by the server to all clients in a room/game to update their game state.
- * @param gameDto the game state to be updated. This object can be used for visualization of the game.
+ * @param roomDto the game state to be updated. This object can be used for visualization of the game.
  */
 public record ServerSyncClientRoomMessage(RoomDTO roomDto) implements Message {
 }
